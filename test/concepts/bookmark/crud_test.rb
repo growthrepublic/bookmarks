8d92f3da833b9be281be3dba10ae2a38196781cd
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2015-05-08 13:25:22 +0200

    Add test for invalid bookmark

--- test/concepts/bookmark/crud_test.rb
+++ test/concepts/bookmark/crud_test.rb
@@ -1,20 +1,28 @@
 require 'test_helper'
 
 class BookmarkCrudTest < MiniTest::Spec
-  describe "Create" do
-    it "persists valid" do
-      bookmark = Bookmark::Create[bookmark:
-          {
-              url: 'http://www.google.pl',
-              title: 'Google Search Engine',
-              description: ''
-          }
-      ].model
+  describe 'Create' do
+    it 'persists valid' do
+      bookmark = Bookmark::Create[bookmark:{
+          url: 'http://www.google.pl',
+          title: 'Google Search Engine',
+          description: ''
+      }].model
 
       bookmark.persisted?.must_equal true
       bookmark.url.must_equal 'http://www.google.pl'
       bookmark.title.must_equal 'Google Search Engine'
       bookmark.description.must_equal ''
+    end
+
+    it 'invalid' do
+      res, op = Bookmark::Create.run(bookmark: {
+          url: ''
+      })
+
+      res.must_equal false
+      op.model.persisted?.must_equal false
+      op.contract.errors.to_s.must_equal '{:url=>["can\'t be blank"]}'
     end
   end
 end
